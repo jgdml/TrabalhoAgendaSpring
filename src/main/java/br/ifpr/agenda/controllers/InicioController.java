200815c2d59e3d2b22ffec1c2f135ab6401de688
jgdml <jgdml2801@gmail.com>
2021-10-26 19:49:55 -0300

    Tela básica de cadastro

--- src/main/java/br/ifpr/agenda/controllers/InicioController.java
+++ src/main/java/br/ifpr/agenda/controllers/InicioController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InicioController {
 
-
     @GetMapping("/login")
     public String getLoginPage(){
         return "inicial/login";
+    }
+
+
+    @GetMapping("/cadastro")
+    public String getCadastroPage(){
+        return "inicial/cadastro";
     }
 }
--- src/main/java/br/ifpr/agenda/security/SecurityConfig.java
+++ src/main/java/br/ifpr/agenda/security/SecurityConfig.java
@@ -32,7 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable().authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/cadastro")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
