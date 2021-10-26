4de553cf0430492ba383a1d0ee746f7516cdc0c9
Kairo <kairo.controlenamao@gmail.com>
2021-10-26 19:11:00 -0300

    Correção

--- src/main/java/br/ifpr/agenda/bootstrap/InicializarDados.java
+++ src/main/java/br/ifpr/agenda/bootstrap/InicializarDados.java
@@ -2,6 +2,8 @@ package br.ifpr.agenda.bootstrap;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,29 +18,33 @@ import br.ifpr.agenda.repositories.ContatoRepository;
 @Component
 public class InicializarDados implements CommandLineRunner {
 
-	@Autowired
-	private ContatoRepository contatoRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Contato contato1 = Contato.builder().nome("João").build();
-		contato1.setSobrenome("Silva");
-		contato1.setEmail("joao.silva@gmail.com");
-		contato1.setDataNascimento(LocalDate.of(1980, Month.OCTOBER, 1));
-		
-		Endereco endereco1 = new Endereco();
-		endereco1.setEnderecoLinha1("Rua Brasil, 123. Cidade Alta");
-		endereco1.setMunicipio("Curitiba");
-		endereco1.setEstado("Paraná");
-		contato1.addEndereco(endereco1);
-		
-		Telefone telefone1 = new Telefone();
-		telefone1.setNumero("(44) 2222-2222");
-		telefone1.setTipo(TipoTelefone.CASA);
-		contato1.addTelefone(telefone1);
-		
-		contatoRepository.save(contato1);
-	}
+    @Autowired
+    private ContatoRepository contatoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Contato contato1 = Contato.builder().nome("João").build();
+        contato1.setSobrenome("Silva");
+        contato1.setEmail("joao.silva@gmail.com");
+        contato1.setDataNascimento(LocalDate.of(1980, Month.OCTOBER, 1));
+
+        Endereco endereco1 = new Endereco();
+        endereco1.setEnderecoLinha1("Rua Brasil, 123. Cidade Alta");
+        endereco1.setMunicipio("Curitiba");
+        endereco1.setEstado("Paraná");
+        List<Endereco> enderecos = new ArrayList<>();
+        enderecos.add(endereco1);
+        contato1.setEnderecos(enderecos);
+
+        Telefone telefone1 = new Telefone();
+        telefone1.setNumero("(44) 2222-2222");
+        telefone1.setTipo(TipoTelefone.CASA);
+        List<Telefone> telefones = new ArrayList<>();
+        telefones.add(telefone1);
+        contato1.setTelefones(telefones);
+
+        contatoRepository.save(contato1);
+    }
 
 }
